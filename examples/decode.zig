61e7feafdb3ec60c71040cb37a7199ebed71aa86
Louis Pearson <opensource@louispearson.work>
2022-09-27 21:37:52 -0600

    Switch to using libFLAC callback API

--- examples/decode.zig
+++ examples/decode.zig
@@ -22,13 +22,11 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
-    std.debug.print("here", .{});
     if (args.len != 3) {
         try stdout.print("version {s}\n", .{version});
         try stdout.print("usage: {s} infile.flac outfile.wav\n", .{args[0]});
         return;
     }
-    std.debug.print("here", .{});
 
     const cwd = std.fs.cwd();
     const in_filename = args[1];
@@ -41,29 +39,81 @@ pub fn main() !void {
 
     // Initialize user data struct to use in callbacks
     var user_data = UserData{
+        // Open input file
+        .in = try cwd.openFile(in_filename, .{}),
         // Open output file
         .out = try cwd.createFile(out_filename, .{}),
     };
     defer user_data.out.close();
+    defer user_data.in.close();
 
     _ = c.FLAC__stream_decoder_set_md5_checking(decoder, 1);
 
-    init_status = c.FLAC__stream_decoder_init_file(decoder, in_filename.ptr, write_callback, metadata_callback, error_callback, &user_data);
+    init_status = c.FLAC__stream_decoder_init_stream(
+        decoder,
+        read_callback,
+        seek_callback,
+        tell_callback,
+        length_callback,
+        eof_callback,
+        write_callback,
+        metadata_callback,
+        error_callback,
+        &user_data,
+    );
     if (init_status != c.FLAC__STREAM_DECODER_INIT_STATUS_OK) return error.InitializingDecoder;
 
     const ok = c.FLAC__stream_decoder_process_until_end_of_stream(decoder) != 0;
     try stdout.print("decoding: {s}", .{if (ok) "succeeded" else "FAILED"});
-    const state_str = std.mem.sliceTo(c.FLAC__StreamDecoderStateString[c.FLAC__stream_decoder_get_state(decoder)], 0);
-    try stdout.print("   state: {s}", .{state_str});
 }
 
 const UserData = struct {
+    in: std.fs.File,
     out: std.fs.File,
     total_samples: u64 = 0,
     sample_rate: u32 = 0,
     channels: u32 = 0,
     bps: u32 = 0,
 };
+
+export fn read_callback(decoder: ?*const c.FLAC__StreamDecoder, buffer_opt: ?[*]c.FLAC__byte, bytes_opt: ?*usize, client_data: ?*anyopaque) callconv(.C) c.FLAC__StreamDecoderReadStatus {
+    _ = decoder;
+    const data = @ptrCast(*UserData, @alignCast(@alignOf(UserData), client_data.?));
+    var reader = data.in.reader();
+    var bytes_ptr = bytes_opt orelse return c.FLAC__STREAM_DECODER_READ_STATUS_ABORT;
+    var buffer = buffer_opt.?[0..bytes_ptr.*];
+    bytes_ptr.* = reader.read(buffer) catch return c.FLAC__STREAM_DECODER_READ_STATUS_ABORT;
+    if (bytes_ptr.* == 0) return c.FLAC__STREAM_DECODER_READ_STATUS_END_OF_STREAM;
+    return c.FLAC__STREAM_DECODER_READ_STATUS_CONTINUE;
+}
+export fn seek_callback(decoder: ?*const c.FLAC__StreamDecoder, absolute_byte_offset: u64, client_data: ?*anyopaque) callconv(.C) c.FLAC__StreamDecoderSeekStatus {
+    _ = decoder;
+    const data = @ptrCast(*UserData, @alignCast(@alignOf(UserData), client_data.?));
+
+    data.in.seekTo(absolute_byte_offset) catch {
+        return c.FLAC__STREAM_DECODER_SEEK_STATUS_ERROR;
+    };
+    return c.FLAC__STREAM_DECODER_SEEK_STATUS_OK;
+}
+export fn tell_callback(decoder: ?*const c.FLAC__StreamDecoder, absolute_byte_offset: ?*u64, client_data: ?*anyopaque) callconv(.C) c.FLAC__StreamDecoderTellStatus {
+    _ = decoder;
+    const data = @ptrCast(*UserData, @alignCast(@alignOf(UserData), client_data.?));
+    absolute_byte_offset.?.* = data.in.getPos() catch return c.FLAC__STREAM_DECODER_TELL_STATUS_ERROR;
+    return c.FLAC__STREAM_DECODER_TELL_STATUS_OK;
+}
+export fn length_callback(decoder: ?*const c.FLAC__StreamDecoder, stream_length: ?*u64, client_data: ?*anyopaque) callconv(.C) c.FLAC__StreamDecoderLengthStatus {
+    _ = decoder;
+    const data = @ptrCast(*UserData, @alignCast(@alignOf(UserData), client_data.?));
+    stream_length.?.* = data.in.getEndPos() catch return c.FLAC__STREAM_DECODER_LENGTH_STATUS_ERROR;
+    return c.FLAC__STREAM_DECODER_LENGTH_STATUS_OK;
+}
+export fn eof_callback(decoder: ?*const c.FLAC__StreamDecoder, client_data: ?*anyopaque) callconv(.C) c.FLAC__bool {
+    _ = decoder;
+    const data = @ptrCast(*UserData, @alignCast(@alignOf(UserData), client_data.?));
+    const pos = data.in.getPos() catch return 1;
+    const end_pos = data.in.getEndPos() catch return 1;
+    return @boolToInt(pos == end_pos);
+}
 
 export fn write_callback(decoder: ?*const c.FLAC__StreamDecoder, frame_opt: ?*const c.FLAC__Frame, buffer_opt: ?[*]const ?[*]const c.FLAC__int32, client_data: ?*anyopaque) callconv(.C) c.FLAC__StreamDecoderWriteStatus {
     const data = @ptrCast(*UserData, @alignCast(@alignOf(UserData), client_data.?));
@@ -162,5 +212,5 @@ export fn error_callback(decoder: ?*const c.FLAC__StreamDecoder, status: c.FLAC_
     _ = decoder;
     _ = client_data;
 
-    std.log.err("Got error callback: {s}", .{c.FLAC__StreamDecoderErrorStatusString[status]});
+    std.log.err("Got error callback: {}", .{status});
 }
